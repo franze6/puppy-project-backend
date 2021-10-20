b275597799953a0146831f8b5a70d6d708823fdc
Derbentsov Dmitriy <dderbentsov@gmail.com>
2021-10-20 16:47:29 +0300

    WIP Create Address, Passport, Messenger

--- puppy/services.py
+++ puppy/services.py
@@ -1,4 +1,4 @@
-from puppy.models import Person
+from puppy.models import Person, Address, Messenger, Passport
 from datetime import date
 
 def person_create(
@@ -50,3 +50,66 @@ def person_update(
 def person_delete(id: str) -> None:
     Person.objects.filter(id=id).delete()
     return None
+
+def address_create(
+                    *, 
+                    address_plain: str,
+                    is_active: bool,
+                    person_id: str, 
+                    #company_id:str=None,
+                  ) -> Address:
+
+  obj = Address(
+    address_plain=address_plain,
+    is_active=is_active,
+    person_id=person_id,
+  )
+
+  #obj.full_clean()
+  obj.save()
+
+  return obj
+
+def messenger_create(
+                    *, 
+                    name: str,
+                    is_active: bool,
+                    uid: str, 
+                    person_id:str,
+                  ) -> Messenger:
+
+  obj = Messenger(
+    name=name,
+    is_active=is_active,
+    uid=uid, 
+    person_id=person_id,
+  )
+
+  #obj.full_clean()
+  obj.save()
+
+  return obj
+
+def passport_create(
+                    *, 
+                    series: str,
+                    number: str,
+                    issued_date: date, 
+                    issued_by: str,
+                    issued_by_code: str ,
+                    person_id: str ,
+                  ) -> Passport:
+
+  obj = Passport(
+    series=series,
+    number=number,
+    issued_date=issued_date, 
+    issued_by=issued_by,
+    issued_by_code=issued_by_code,
+    person_id=person_id,
+  )
+
+  #obj.full_clean()
+  obj.save()
+
+  return obj
--- puppy/urls.py
+++ puppy/urls.py
@@ -10,7 +10,9 @@ from .views import (
     PersonListApi,
     PersonCreateApi,
     PersonUpdateApi,
-
+    AddressCreateApi,
+    MessengerCreateApi,
+    PassportCreateApi,
 )
 
 app_name = 'puppy'
@@ -24,8 +26,38 @@ person_patterns = [
     path('<int:person_id>/delete/', PersonDeleteApi.as_view(), name='delete')
 ]
 
+address_patterns = [
+    #path('', PersonListApi.as_view(), name='list'),
+    #path('', PersonsView.as_view(), name='list'),
+    #path('<int:pk>/', PersonDetailView.as_view(), name='detail'),
+    path('create/', AddressCreateApi.as_view(), name='create'),
+    #path('<int:person_id>/update/', PersonUpdateApi.as_view(), name='update'),
+    #path('<int:person_id>/delete/', PersonDeleteApi.as_view(), name='delete')
+]
+
+messenger_patterns = [
+    #path('', PersonListApi.as_view(), name='list'),
+    #path('', PersonsView.as_view(), name='list'),
+    #path('<int:pk>/', PersonDetailView.as_view(), name='detail'),
+    path('create/', MessengerCreateApi.as_view(), name='create'),
+    #path('<int:person_id>/update/', PersonUpdateApi.as_view(), name='update'),
+    #path('<int:person_id>/delete/', PersonDeleteApi.as_view(), name='delete')
+]
+
+passport_patterns = [
+    #path('', PersonListApi.as_view(), name='list'),
+    #path('', PersonsView.as_view(), name='list'),
+    #path('<int:pk>/', PersonDetailView.as_view(), name='detail'),
+    path('create/', PassportCreateApi.as_view(), name='create'),
+    #path('<int:person_id>/update/', PersonUpdateApi.as_view(), name='update'),
+    #path('<int:person_id>/delete/', PersonDeleteApi.as_view(), name='delete')
+]
+
 urlpatterns = [
-    path('persons/', include((person_patterns, 'persons')))
+    path('persons/', include((person_patterns, 'persons'))),
+    path('addresses/', include((address_patterns, 'addresses'))),
+    path('messengers/', include((messenger_patterns, 'messengers'))),
+    path('passports/', include((passport_patterns, 'passports'))),
     #path('person/<int:pk>/', PersonDetailView.as_view(), name='person-datail'),
     #path('person-api/<int:pk>/', PersonUpdateApi.as_view(), name='person-api'),
     #path('person-del/<int:pk>/', PersonDeleteApi.as_view(), name='person-del'),
